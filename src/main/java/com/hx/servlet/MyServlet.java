104de236c1e1fd9fa172e9ad70dbd94175c748d5
cyz <chenyongzhijjk@163.com>
2019-07-17 13:01:06 +0800

    Initial commit

---
+++ src/main/java/com/hx/servlet/MyServlet.java
@@ -0,0 +1,21 @@
+package com.hx.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author chenyongzhi
+ * @create 2019/7/17 10:10
+ */
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hxzy
+  Date: 2019/7/17
+  Time: 10:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    hello maven
+</body>
+</html>
